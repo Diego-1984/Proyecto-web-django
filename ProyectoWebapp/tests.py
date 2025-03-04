56d9c77ce7b23803a5b3919427916a50bb34a4d9
Diego <116676429+Diego-1984@users.noreply.github.com>
2025-03-04 19:29:43 +0100

    Añadido selenium test solventado nav pills cambiado estilo de la web

--- ProyectoWebapp/tests.py
+++ ProyectoWebapp/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+class PruebasFuncionales(LiveServerTestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()  # Usa ChromeDriver
+
+    def tearDown(self):
+        self.driver.quit()  # Cierra el navegador al terminar
+
+    def test_pagina_inicio(self):
+        # Abrir la página en el navegador
+        self.driver.get(self.live_server_url)
+
+        # Buscar un elemento por su etiqueta <h1>
+        encabezado = self.driver.find_element(By.TAG_NAME, "h1")
+
+        # Verificar que el título de la página es correcto
+        self.assertEqual(encabezado.text, "Gestión de Pedidos")
